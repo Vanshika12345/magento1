c0347c37e7b7b898ed59094bc61950bf81f8dc4f
Vanshika <vanshikaagarwal544@gmail.com>
2021-05-20 19:30:56 +0530

    Payment Method added

---
+++ magento_ccc/app/code/local/Ccc/MyPaymentMethod/Model/Standard.php
@@ -0,0 +1,18 @@
+<?php
+ 
+class Ccc_MyPaymentMethod_Model_Standard extends Mage_Payment_Model_Method_Abstract{
+ 
+	protected $_code = 'mypaymentmethod';
+	 
+	/*protected $_isInitializeNeeded      = true;
+	protected $_canUseInternal          = false;
+	protected $_canUseForMultishipping  = false;
+	 
+	public function getOrderPlaceRedirectUrl()
+	{
+		return Mage::getUrl('mypaymentmethod/standard/redirect', array('_secure' => true));
+	}*/
+ 
+}
+
+?>
