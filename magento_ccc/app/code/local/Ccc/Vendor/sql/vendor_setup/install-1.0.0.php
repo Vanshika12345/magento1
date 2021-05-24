ba601749e1f989447486105511fbcaebe2b61e82
Vanshika <vanshikaagarwal544@gmail.com>
2021-05-24 18:19:12 +0530

    Changed1

--- magento_ccc/app/code/local/Ccc/Vendor/sql/vendor_setup/install-1.0.0.php
+++ magento_ccc/app/code/local/Ccc/Vendor/sql/vendor_setup/install-1.0.0.php
@@ -1,31 +1,31 @@
 <?php
 
-
-
-$this->startSetup();
-$this->run( 
-	"DROP TABLE IF EXISTS `vendor_int`;
-    DROP TABLE IF EXISTS `vendor_varchar`;
-    DROP TABLE IF EXISTS `vendor_datetime`;
-    DROP TABLE IF EXISTS `vendor_decimal`;
-    DROP TABLE IF EXISTS `vendor_text`;
-    DROP TABLE IF EXISTS `vendor_char`;
-    DROP TABLE IF EXISTS `vendor_eav_attribute`;
-    DROP TABLE IF EXISTS `vendor`;
+$installer = $this;
+$installer->run( 
+	"DROP TABLE IF EXISTS `{$installer->getTable('vendor_int')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_varchar')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_datetime')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_decimal')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_text')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_char')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_eav_attribute')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor')}`;
     DELETE FROM `eav_entity_type` WHERE `entity_type_code` IN('vendor');
-    DROP TABLE IF EXISTS `vendor_product_entity_int`;
-    DROP TABLE IF EXISTS `vendor_product_entity_varchar`;
-    DROP TABLE IF EXISTS `vendor_product_entity_datetime`;
-    DROP TABLE IF EXISTS `vendor_product_entity_decimal`;
-    DROP TABLE IF EXISTS `vendor_product_entity_text`;
-    DROP TABLE IF EXISTS `vendor_product_entity_gallery`;
-    DROP TABLE IF EXISTS `vendor_product_eav_attribute`;
-    DROP TABLE IF EXISTS `vendor_product_entity`;
-    DROP TABLE IF EXISTS `vendor_product_group`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_int')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_varchar')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_datetime')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_decimal')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_text')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity_gallery')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_eav_attribute')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_entity')}`;
+    DROP TABLE IF EXISTS `{$installer->getTable('vendor_product_group')}`;
     DELETE FROM `eav_entity_type` WHERE `entity_type_code` IN('vendor_product');
      DELETE FROM `core_resource` WHERE `code` = 'vendor_setup';
     "
 	);
+
+$this->startSetup();
 $this->addEntityType(Ccc_Vendor_Model_Resource_Vendor::ENTITY, [
 	'entity_model' => 'vendor/vendor',
 	'attribute_model' => 'vendor/attribute',
